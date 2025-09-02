0584ba1fe70aba34fa1a1da62a1ead85bca06eef
Enoughsdv <enoughsdv@gmail.com>
2025-09-01 19:01:53 -0500

    Refactor password generation logic to validate input length and include additional symbols

--- lambda/password-generator/index.mjs
+++ lambda/password-generator/index.mjs
@@ -2,10 +2,17 @@
  * Generador de contraseñas
  * { length: <número> } - máximo 64 caracteres y mínimo 8 caracteres
  */
+// length = 10
 export const handler = async (event) => {
     const { length } = event;
 
-    if (length < 8 || length > 64) {
+    if (isNaN(length)) {
+        return returnResult({ error: 'Entrada inválida', hint: 'Introduce un número del 8 al 64' }, 400);
+    }
+
+    const validLength = Math.floor(parseInt(length));
+
+    if (validLength < 8 || validLength > 64) {
         return returnResult({ error: 'Longitud no válida', hint: 'La longitud debe estar entre 8 y 64 caracteres' }, 400);
     }
 
@@ -13,12 +20,12 @@ export const handler = async (event) => {
     const upperCase = lowerCase.toUpperCase();
     const numbers = '1234567890';
 
-    const symbols = '!#$%&/()=?¿¡.;-_{}[]';
+    const symbols = '!#$%&/()=?¿¡.;-_{}[]*`';
 
-    const allCharacters = lowerCase + upperCase + numbers + symbols;
+    const allCharacters = lowerCase + upperCase + numbers + symbols; // 84
 
     let password = '';
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < validLength; i++) {
         const randomIndex = Math.floor(Math.random() * allCharacters.length);
         password += allCharacters[randomIndex];
     }
